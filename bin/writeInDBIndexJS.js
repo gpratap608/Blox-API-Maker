a7927d2d6867649e626477601f746b517301af2e
gpratap608 <113552039+gpratap608@users.noreply.github.com>
2024-06-10 00:15:17 +0530

    CLI changes

--- bin/writeInDBIndexJS.js
+++ bin/writeInDBIndexJS.js
@@ -10,8 +10,8 @@ import { DB_NAME } from "../constants.js"
 
 const connectDB = async () =>{
 try {
-    const connectInstance = await mongoose.connect("a{process.env.MONGO_URI}/a{DB_NAME}")   //change "" to `` and a to $
-    console.log("\n MongoDB Connected !! DB host : a{connectInstance.connection.host}"); // change "" to `` and a to $
+    const connectInstance = await mongoose.connect("a{process.env.MONGO_URI}/a{DB_NAME}")   //change "" to BACKTICKS and a to $
+    console.log("MongoDB Connected !! DB host : a{connectInstance.connection.host}"); // change "" to BACKTICKS and a to $
 } catch (error) {
     console.log("Error: "+ error)
     process.exit(1)
